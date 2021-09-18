dacf761c385395231d218bd68eeb3e7148e3ffe5
wasabi <wasabi.c93@gmail.com>
2021-09-18 21:11:38 +0700

    sat 18 sep 21:11 2021

---
+++ components/Lamb.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+import home from "../styles/home.module.sass";
+
+import turnOn from "../public/lamp/lamp_on.png";
+import turnOff from "../public/lamp/lamp_off.png";
+
+export default function Lamb() {
+  const [switchLamb, setSwitchLamb] = useState(true);
+
+  function handleSwitchLamb() {
+    setSwitchLamb(!switchLamb);
+  }
+
+
+  return (
+    <div className={home.lamb}>
+      <div className={home.lambSwitch} onClick={handleSwitchLamb}></div>
+      <div>
+        {switchLamb ? (
+          <div className={home.turnOn}>
+            <Image src={turnOn} alt="" layout="responsive" />
+          </div>
+        ) : (
+          <div className={home.turnOff}>
+            <Image src={turnOff} alt="" layout="responsive" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- components/newHP.js
+++ components/newHP.js
@@ -1,6 +1,7 @@
 import home from "../styles/home.module.sass";
 import Clock from "./Clock";
-import Books from './Books'
+import Books from './Books';
+import Lamb from './Lamb'
 
 export default function NewHP() {
   return (
@@ -12,6 +13,9 @@ export default function NewHP() {
           </div>
           <div className={home.booksContainer}>
             <Books />
+          </div>
+          <div className={home.lambContainer}>
+            <Lamb />
           </div>
         </div>
       </div>
