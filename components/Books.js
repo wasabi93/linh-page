02ec3150227c065b1a150d5bcf4f00b370251055
wasabi <wasabi.c93@gmail.com>
2021-09-19 11:30:50 +0700

    sun 19 sep 11:30 2021

--- components/Books.js
+++ components/Books.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 import home from "../styles/home.module.sass";
 
-import yellowOne from "../public/books/yellow_book_below.png";
-import blueOne from "../public/books/dark_blue_book_above.png";
-import redOne from "../public/books/red_book.png";
+import yellowOne from "../public/books/book_yellow.png";
+import blueOne from "../public/books/book_blue.png";
+import redOne from "../public/books/book_red.png";
 import pinkOne from "../public/books/light_pink_book.png";
-import yellowTwo from "../public/books/yellow_book_right.png";
+import yellowTwo from "../public/books/book_yellow_right.png";
 
 export default function Books() {
   return (
