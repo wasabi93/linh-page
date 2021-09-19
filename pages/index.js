907cd859ab5d31ca3caf74365237f9be1b7f26e5
wasabi <wasabi.c93@gmail.com>
2021-09-19 22:01:45 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function HomePage() {
     <div className={home.container}>
       <Head>
         <title>HomePage</title>
+        <meta property="og:image" content="https://media.giphy.com/media/Utrfj8quziX6XQ3Tc5/giphy.gif?cid=790b7611a03a4d5da18f9b1284c8efe61d68b9901c7f397d&rid=giphy.gif&ct=g"></meta>
+        <meta property="og:image:type" content="image/jpeg"></meta>
+        <meta property="og:image:width" content="200"></meta>
+        <meta property="og:image:height" content="200"></meta>
       </Head>
       <NewHP />
     </div>
