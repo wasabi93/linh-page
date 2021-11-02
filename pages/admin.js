160dfc2190071379b5282f109b3f9a4a82b1cc2a
wasabi <wasabi.c93@gmail.com>
2021-11-02 16:08:07 +0700

    update unmounted

--- pages/admin.js
+++ pages/admin.js
@@ -2,32 +2,38 @@ import Head from "next/head";
 import { withAuthSync } from "../lib/auth";
 import { useState, useEffect } from "react";
 
-import Admin from '../components/Admin'
+import Admin from "../components/Admin";
 import admin from "../styles/admin.module.sass";
 
 const adminPage = () => {
-  const [posts,setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-  const getPosts = async() => {
-    const res = await fetch('/api/posts')
-    const posts = await res.json()
-    return posts.data
-  }
+  const getPosts = async () => {
+    const res = await fetch("/api/posts");
+    const posts = await res.json();
+    return posts.data;
+  };
 
   useEffect(() => {
-    getPosts().then((data) => setPosts(data)).catch(e => console.log(e))
-  },[posts])
-
+    let isMounted = true;
+    getPosts()
+      .then((data) => {
+        if (isMounted) setPosts(data);
+      })
+      .catch((e) => console.log(e));
+    return () => {
+      isMounted = false;
+    };
+  }, [posts]);
 
   return (
     <div className={admin.container}>
       <Head>
         <title>Admin</title>
       </Head>
-      <Admin posts={posts}/>
+      <Admin posts={posts} />
     </div>
   );
-}
-
+};
 
 export default withAuthSync(adminPage);
